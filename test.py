3cf441ac48b92b0c1453a73a0adad163da1c8cb3
fermibot <ashwinuoh@gmail.com>
2018-07-29 21:14:57 -0500

    new exercise directory

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from mathematica.load_all_functions import *
 
-for i in range(0, 10):
+for i in range(0, 20):
     __size = 200
     __list = RandomSample(Range(0, __size), __size)
-    QuickSortTrack(__list, 'D:\\Mathematica Files 4K\\sheldon_ross\\sheldon_ross_chapter_03\\sheldon_ross_example_3.16\\sheldon_ross_example_3.16_animation_data_' + f'{i:03}' + '.txt')
+    QuickSortTrack(__list, 'D:\\Mathematica Files 4K\\sheldon_ross\\sheldon_ross_chapter_03\\sheldon_ross_example_3.16\\animation_data\\sheldon_ross_example_3.16_animation_data_' + f'{i:03}' + '.txt')
