be8f4bc28eee9cda4d39e0baefc0b47471d0dc76
fermibot <ashwinuoh@gmail.com>
2018-08-02 19:02:43 -0500

    added the worst gift without

--- mathematica/sheldon_ross_problems/sheldon_ross_example_3.25.py
+++ mathematica/sheldon_ross_problems/sheldon_ross_example_3.25.py
@@ -7,32 +7,67 @@ unknownPath = open('folderPath.txt').readlines()[0]
 chapterName = 'sheldon_ross_chapter_03'
 problemName = 'sheldon_ross_example_3.25'
 
-for variableGifts in [10, 50, 100]:
-    numberOfGifts = variableGifts
 
-    for rejInd in range(0, numberOfGifts):
-        __fileName = ConstructFileName([unknownPath, chapterName, problemName, problemName], '_' + str(numberOfGifts) + '_'
-                                       + str(rejInd) + '.txt')
-        __file = open(__fileName, 'w')
+def bestGift():
+    for variableGifts in [10, 50, 100]:
+        numberOfGifts = variableGifts
 
-        TimeTagMessage('Opening file ' + __fileName)
-        TimeTagMessage('Writing into file')
+        for rejInd in range(0, numberOfGifts):
+            __fileName = ConstructFileName([unknownPath, chapterName, problemName, problemName], '_' + str(numberOfGifts) + '_'
+                                           + str(rejInd) + '.txt')
+            __file = open(__fileName, 'w')
 
-        for r in range(0, 1000000):
-            randomSample = RandomSample(Range(numberOfGifts), numberOfGifts)
-            rejectedIndex = rejInd
-            rejectedSequence = randomSample[0:rejectedIndex]
-            unRejectedSequence = randomSample[rejectedIndex:len(randomSample)]
+            TimeTagMessage('Opening file ' + __fileName)
+            TimeTagMessage('Writing into file')
 
-            accepted = None
-            for i in unRejectedSequence:
-                if GreaterThanAll(i, rejectedSequence):
-                    accepted = i
-                    break
-            __file.write(str(accepted) + '\n')
+            for r in range(0, 1000000):
+                randomSample = RandomSample(Range(numberOfGifts), numberOfGifts)
+                rejectedIndex = rejInd
+                rejectedSequence = randomSample[0:rejectedIndex]
+                unRejectedSequence = randomSample[rejectedIndex:len(randomSample)]
 
-        TimeTagMessage('Closing the file ' + __fileName)
-        __file.close()
-        print('')
+                accepted = None
+                for i in unRejectedSequence:
+                    if GreaterThanAll(i, rejectedSequence):
+                        accepted = i
+                        break
+                __file.write(str(accepted) + '\n')
 
+            TimeTagMessage('Closing the file ' + __fileName)
+            __file.close()
+            print('')
+
+
+def worstGift():
+    for variableGifts in [10, 50, 100]:
+        numberOfGifts = variableGifts
+
+        for rejInd in range(0, numberOfGifts):
+            __fileName = ConstructFileName([unknownPath, chapterName, problemName, problemName],
+                                           '_worst_' + str(numberOfGifts) + '_'
+                                           + str(rejInd) + '.txt')
+            __file = open(__fileName, 'w')
+
+            TimeTagMessage('Opening file ' + __fileName)
+            TimeTagMessage('Writing into file')
+
+            for r in range(0, 1000000):
+                randomSample = RandomSample(Range(numberOfGifts), numberOfGifts)
+                rejectedIndex = rejInd
+                rejectedSequence = randomSample[0:rejectedIndex]
+                unRejectedSequence = randomSample[rejectedIndex:len(randomSample)]
+
+                accepted = None
+                for i in unRejectedSequence:
+                    if LessThanAll(i, rejectedSequence):
+                        accepted = i
+                        break
+                __file.write(str(accepted) + '\n')
+
+            TimeTagMessage('Closing the file ' + __fileName)
+            __file.close()
+            print('')
+
+bestGift()
+worstGift()
 
