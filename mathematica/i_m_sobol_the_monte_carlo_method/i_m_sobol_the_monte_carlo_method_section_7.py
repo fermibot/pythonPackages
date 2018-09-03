8edb871a8bb85539fd90a3da87e51f335f0a6b43
fermibot <ashwinuoh@gmail.com>
2018-09-03 11:29:24 -0500

    Added I. M. Sobol the monte carlo method section 7

---
+++ mathematica/i_m_sobol_the_monte_carlo_method/i_m_sobol_the_monte_carlo_method_section_7.py
@@ -0,0 +1,6 @@
+from mathematica.random_functions import *
+from mathematica.lists import *
+from mathematica.monitoring import *
+from mathematica.sheldon_ross_problems.sheldon_ross_utilities import *
+
+
