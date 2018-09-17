3bbf1b7e9fe5e5c0bb99ed0b24041fee6bbc2ab5
fermibot <ashwinuoh@gmail.com>
2018-09-17 18:54:57 -0500

    committing  test file just in case

--- test.py
+++ test.py
@@ -1,4 +1,19 @@
 from mathematica.load_all_functions import *
 
-for i in Range(0, Pi(200), 0.1):
-    print([Sin(i), Cos(i), Tan(i), Sec(i), Csc(i), Cot(i)])
+# for i in Range(0, Pi(200), 0.1):
+#    print([Sin(i), Cos(i), Tan(i), Sec(i), Csc(i), Cot(i)])
+
+import csv
+import time
+
+with open('text.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        print(row)
+        for element in row:
+            print(element)
+
+for x in range(0, 5):
+    b = "Loading" + "." * x + "\r"
+    print(b)
+    time.sleep(0.2)
