6777671b31829ffe144761ad6a5adeee32a05485
fermibot <ashwinuoh@gmail.com>
2018-10-15 06:29:37 -0500

    Fully working version of Exercise 3.08 and this one exports the processed lists as a .txt file

--- mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.08.py
+++ mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.08.py
@@ -1,22 +1,26 @@
 from mathematica.random_functions import RandomChoice, RandomInteger
-from mathematica.lists import MemberQ
 
-_list = []
-_truth = True
-_i = 1
+_masterList = []
+for r in range(0, 100000):
+    _list = []
+    _i = 1
 
-while _truth:
-    if _i == 5:
-        _list.append(5)
-    elif _i < 6:
-        _rand = RandomChoice([1, 2, 3, 4, 6])
-        _list.append(_rand)
-    elif _i > 6:
-        _rand = RandomInteger(1, 6)
-        if _rand != 6:
+    while True:
+        if _i == 5:
+            _list.append(5)
+        elif _i < 6 and _i != 5:
+            _rand = RandomChoice([1, 2, 3, 4, 6])
             _list.append(_rand)
-        else:
-            _list.append(_rand)
-            break
-    _i = _i + 1
-    print(_list)
+        elif _i >= 6:
+            _rand = RandomInteger(1, 6)
+            if _rand != 6:
+                _list.append(_rand)
+            elif _rand == 6:
+                _list.append(_rand)
+                break
+        _i = _i + 1
+    _masterList.append(_list.index(6) + 1)
+
+_file = open('D:\Mathematica Files 4K\sheldon_ross\sheldon_ross_chapter_03\sheldon_ross_exercise_3.08\sheldon_ross_10_exercise_3.08.txt', 'w')
+for r in _masterList:
+    _file.write(r.__str__() + '\n')
