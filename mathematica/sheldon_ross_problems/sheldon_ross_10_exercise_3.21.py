f6f018b9b2e92cb56f31cd129b1d2c0414ba7248
fermibot <ashwinuoh@gmail.com>
2018-10-21 09:00:49 -0500

    Updates to exercise 3.08, adding the temporary print for looking at progress

--- mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.21.py
+++ mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.21.py
@@ -1,7 +1,13 @@
+from sys import stdout
 from mathematica.random_functions import RandomChoice, RandomReal
+from mathematica.lists import Transpose
+from mathematica.string_operations import StringPadLeft, StringPadRight
 
-_nList, _timeList = [], []
-for i in range(0, 1000):
+_file = open(
+    'D:\Mathematica Files 4K\sheldon_ross\sheldon_ross_chapter_03\sheldon_ross_exercise_3.21\sheldon_ross_10_exercise_3.21.txt',
+    'w')
+
+for i in range(0, 1000000):
     _n, _time = 0, 0
     while True:
         _n += 1
@@ -13,7 +19,8 @@ for i in range(0, 1000):
             _time += 3
         elif 2 / 3 <= _rand <= 1:
             _time += 5
-    _timeList.append(_time)
-    _nList.append(_n)
-print(sum(_timeList) / _timeList.__len__())
-print(sum(_nList) / _nList.__len__())
+    _file.write({_n, _time}.__str__() + '\n')
+    if (i + 1) % 1000 == 0:
+        # stdout.write("\rWriting line #" + str(i + 1) + " to the file.")
+        stdout.write( "\r" + ("#" * (i // 10000)))
+        stdout.flush()
--- mathematica/string_operations.py
+++ mathematica/string_operations.py
@@ -68,6 +68,14 @@ def ToLowerCase(*args):
         return _toLowerCase
 
 
+def StringPadLeft(_str, *args):
+    pass
+
+
+def StringPadRight(_str, *args):
+    pass
+
+
 def Alphabet():
     return ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'v', 'x', 'y', 'z']
