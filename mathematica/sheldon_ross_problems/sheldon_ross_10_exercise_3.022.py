98cc0e8d77563121fae0e7216a7928f00b28f0bf
fermibot <ashwinuoh@gmail.com>
2018-10-23 20:42:35 -0500

    Updates to 3.022

--- mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.022.py
+++ mathematica/sheldon_ross_problems/sheldon_ross_10_exercise_3.022.py
@@ -3,31 +3,32 @@ import sys
 from mathematica.lists import EqualAll, Range
 from mathematica.random_functions import RandomChoice
 
-_steps = 10000
-_subSteps = _steps // 100
-_n = 10
-_set = Range(_n)
+for _n in [10, 15]:
+    _steps = 10000
+    _subSteps = _steps // 100
+    __n = _n
+    _set = Range(_n)
 
-for _k in range(2, 5):
-    _progressPad = "_" * 106
-    _file = open(
-        'D:\Mathematica Files 4K\sheldon_ross\sheldon_ross_chapter_03\sheldon_ross_exercise_3.22\sheldon_ross_10_exercise_3.22_' + str(_n) + '_' + str(
-            _k) + '.txt', 'w')
+    for _k in range(2, 5):
+        _progressPad = "_" * 106
+        _file = open(
+            'D:\Mathematica Files 4K\sheldon_ross\sheldon_ross_chapter_03\sheldon_ross_exercise_3.22\sheldon_ross_10_exercise_3.22_' + str(_n) + '_' + str(
+                _k) + '.txt', 'w')
 
-    print("\n\nWriting data to the file :" + _file.name)
-    print(_progressPad)
+        print("\n\nWriting data to the file :" + _file.name)
+        print(_progressPad)
 
-    for i in range(0, _steps):
-        _sample = RandomChoice(_set, _k)
-        while True:
-            if EqualAll(_sample[-_k:]):
-                break
-            else:
-                _sample.append(RandomChoice(_set))
-        _file.write(_sample.__str__().replace("[", "{").replace("]", "}") + '\n')
-        if (i + 1) % _subSteps == 0:
-            stdout.write("\r" + ("\r" + ("#" * (i // _subSteps + 1)).ljust(100)) + " " + str(
-                i // _subSteps + 1) + " %")
-            stdout.flush()
-print("\nClosing the file: " + _file.name)
-_file.close()
+        for i in range(0, _steps):
+            _sample = RandomChoice(_set, _k)
+            while True:
+                if EqualAll(_sample[-_k:]):
+                    break
+                else:
+                    _sample.append(RandomChoice(_set))
+            _file.write(_sample.__str__().replace("[", "{").replace("]", "}") + '\n')
+            if (i + 1) % _subSteps == 0:
+                stdout.write("\r" + ("\r" + ("#" * (i // _subSteps + 1)).ljust(100)) + " " + str(
+                    i // _subSteps + 1) + " %")
+                stdout.flush()
+    print("\nClosing the file: " + _file.name)
+    _file.close()
