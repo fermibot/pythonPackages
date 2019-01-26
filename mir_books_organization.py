fe4a3e89bdbca8ea1be1918b764cbb928324db27
fermibot <ashwinuoh@gmail.com>
2019-01-26 14:35:49 -0600

    added more code for bifurcating author names

--- mir_books_organization.py
+++ mir_books_organization.py
@@ -1,8 +1,8 @@
 from mathematica.string_operations import StringDelete
 from mathematica.databaseLink import SQLExecute, OpenSQLConnection
-from mathematica.q_functions import StringQ
+from mathematica.lists import Last, Riffle
+from mathematica.string_operations import StringJoin
 import os, fnmatch
-import re
 
 
 def find(pattern, path):
@@ -26,6 +26,7 @@ _sqlConnection = OpenSQLConnection('D:\Programming\python\PyCharm\mathematicaPyt
 
 _maxIndex = SQLExecute(_sqlConnection, "select max(authorID) from authors").fetchall()[0][0]
 
+
 # for i in range(0, _booksHave.__len__()):
 #     _sqlString = "INSERT INTO books VALUES (" + str(i + 1 + _maxIndex) + ", '" + _booksHave[i][0] + "', Null)"
 #     _sqlString.replace("'", "\'")
@@ -33,17 +34,27 @@ _maxIndex = SQLExecute(_sqlConnection, "select max(authorID) from authors").fetc
 #     _sqlConnection.cursor().execute(_sqlString)
 #     _sqlConnection.commit()
 
-def nameExtract(_name:str):
-    return _name
+def nameExtract(_name: str):
+    if "." in _name:
+        _name = _name.split('.')
+        _lastName = Last(_name)
+        del _name[-1]
+        return [_name, _lastName]
+    if "." not in _name:
+        return _name.split(' ')
+
 
 for authorList in _booksHave:
     del authorList[0]
     for author in authorList:
         _results = SQLExecute(_sqlConnection,
-                          "select * from authors where firstName||lastName = '" + author + "'").fetchall()
+                              "select * from authors where firstName||lastName = '" + author + "'").fetchall()
         if _results.__len__() == 0:
             print(author + " | Author not found inserting into SQLDatabase")
+            print(nameExtract(author))
+            print("")
             pass
         else:
             print(author + " | Author already exists")
-
+            print(nameExtract(author))
+            print("")
