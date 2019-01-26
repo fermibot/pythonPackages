dbde7a994db4dbe4ce4421b31b142e7c80578488
fermibot <ashwinuoh@gmail.com>
2019-01-26 15:43:28 -0600

    added conditionals for switching the execution s as on and off

--- mir_books_organization.py
+++ mir_books_organization.py
@@ -19,26 +19,12 @@ def find(pattern, path):
 _directory = 'D:\Mir Book Processing'
 _booksHave = []
 books = find('*', _directory)
-# print('The number of books I found is ' + books.__len__().__str__() + "\n")
 for i in books:
     i = StringDelete(i, [_directory, '\\', '.pdf', '.djvu']).split(' - ')
     _booksHave.append(i)
 
 _sqlConnection = OpenSQLConnection('D:\Programming\python\PyCharm\mathematicaPython\collections.db')
-
-for book in _booksHave:
-    book = book[0]
-    _results = SQLExecute(_sqlConnection,
-                          "SELECT * FROM books WHERE bookName = '" + book + "'").fetchall()
-    if _results.__len__() == 0:
-        _id = SQLExecute(_sqlConnection, "SELECT Max(bookID) + 1 FROM books").fetchall()[0][0]
-        TimeTagMessage(book + " | Book not found in the table. Inserting into Authors")
-        _sqlQuery = "INSERT INTO books (bookID, bookName, series) VALUES (" + str(_id) + ", '" + \
-                    book + "', Null)"
-        # _sqlConnection.cursor().execute(_sqlQuery)
-        _sqlConnection.commit()
-    else:
-        TimeTagMessage(book + " | Book already exists in the table. Moving on ;)")
+_updateDatabasesQ = False
 
 
 def nameExtract(_name: str):
@@ -53,21 +39,41 @@ def nameExtract(_name: str):
         return _name.split(' ')
 
 
-'''
-for authorList in _booksHave:
-    del authorList[0]
-    for author in authorList:
-        _nameExtract = nameExtract(author)
+if False:
+    for book in _booksHave:
+        book = book[0]
         _results = SQLExecute(_sqlConnection,
-                              "SELECT * FROM authors WHERE firstName = '" +
-                              _nameExtract[0] + "' AND lastName = '" + _nameExtract[1] + "'").fetchall()
+                              "SELECT * FROM books WHERE bookName = '" + book + "'").fetchall()
         if _results.__len__() == 0:
-            _id = SQLExecute(_sqlConnection, "SELECT Max(authorID) + 1 FROM authors").fetchall()[0][0]
-            TimeTagMessage(author + " | Author not found in the table. Inserting into Authors")
-            _sqlQuery = "INSERT INTO authors (authorID, firstName, lastName) VALUES (" + str(_id) + ", '" + \
-                        _nameExtract[0] + "', '" + _nameExtract[1] + "')"
-            _sqlConnection.cursor().execute(_sqlQuery)
-            _sqlConnection.commit()
+            _id = SQLExecute(_sqlConnection, "SELECT Max(bookID) + 1 FROM books").fetchall()[0][0]
+            TimeTagMessage(book + " | Book not found in the table. Inserting into Authors")
+            _sqlQuery = "INSERT INTO books (bookID, bookName, series) VALUES (" + str(_id) + ", '" + \
+                        book + "', Null)"
+            if _updateDatabasesQ:
+                _sqlConnection.cursor().execute(_sqlQuery)
+                _sqlConnection.commit()
+            elif not _updateDatabasesQ:
+                print(_sqlQuery)
         else:
-            TimeTagMessage(author + " | Author already exists in the table. Moving on ;)")
-'''
+            TimeTagMessage(book + " | Book already exists in the table. Moving on ;)")
+
+if False:
+    for authorList in _booksHave:
+        del authorList[0]
+        for author in authorList:
+            _nameExtract = nameExtract(author)
+            _results = SQLExecute(_sqlConnection,
+                                  "SELECT * FROM authors WHERE firstName = '" +
+                                  _nameExtract[0] + "' AND lastName = '" + _nameExtract[1] + "'").fetchall()
+            if _results.__len__() == 0:
+                _id = SQLExecute(_sqlConnection, "SELECT Max(authorID) + 1 FROM authors").fetchall()[0][0]
+                TimeTagMessage(author + " | Author not found in the table. Inserting into Authors")
+                _sqlQuery = "INSERT INTO authors (authorID, firstName, lastName) VALUES (" + str(_id) + ", '" + \
+                            _nameExtract[0] + "', '" + _nameExtract[1] + "')"
+                if _updateDatabasesQ:
+                    _sqlConnection.cursor().execute(_sqlQuery)
+                    _sqlConnection.commit()
+                elif not _updateDatabasesQ:
+                    print(_sqlQuery)
+            else:
+                TimeTagMessage(author + " | Author already exists in the table. Moving on ;)")
