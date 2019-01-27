f5c526df65f0d65eda73d54767e3e976d1aec126
fermibot <ashwinuoh@gmail.com>
2019-01-27 17:47:41 -0600

    added more tracking code to mir_books_organiation

--- mir_books_organization.py
+++ mir_books_organization.py
@@ -39,17 +39,23 @@ def nameExtract(_name: str):
         return _name.split(' ')
 
 
+print("Found a total of " + str(_booksHave.__len__()) + "\n")
+
 _updateDatabasesQ = True
 
+_bookTrack01 = 1
+_bookLength = _booksHave.__len__()
 TimeTagMessage("Starting Books Upload")
 if True:
     for book in _booksHave:
         book = book[0]
         _results = SQLExecute(_sqlConnection,
                               "SELECT * FROM books WHERE bookName = '" + book + "'").fetchall()
+        _pre = " | Progress " + str(_bookTrack01) + " off " + str(_bookLength)
         if _results.__len__() == 0:
             _id = SQLExecute(_sqlConnection, "SELECT Max(bookID) + 1 FROM books").fetchall()[0][0]
-            TimeTagMessage(" | Book not found in the table. Inserting into books.\t | " + book)
+            TimeTagMessage(" | Progress " + str(_bookTrack01) + " off " + str(
+                _bookLength) + " | Book not found in the table. Inserting into books.\t | " + book)
             _sqlQuery = "INSERT INTO books (bookID, bookName, series) VALUES (" + str(_id) + ", '" + \
                         book + "', NULL)"
             if _updateDatabasesQ:
@@ -58,21 +64,28 @@ if True:
             elif not _updateDatabasesQ:
                 print(_sqlQuery + "\n")
         else:
-            TimeTagMessage(" | Book already exists in the table. Moving on.\t\t\t | " + book)
+            TimeTagMessage(_pre + " | Book already exists in the table. Moving on.\t\t | " + book)
+        _bookTrack01 += 1
 
 print("")
 TimeTagMessage("Starting Authors Upload")
 if True:
+    _bookTrack02 = 1
     for authorList in _booksHave:
         authorList = Rest(authorList)
+        _authorTrack = 1
+        _authorLength = authorList.__len__()
         for author in authorList:
             _nameExtract = nameExtract(author)
             _results = SQLExecute(_sqlConnection,
                                   "SELECT * FROM authors WHERE firstName = '" +
                                   _nameExtract[0] + "' AND lastName = '" + _nameExtract[1] + "'").fetchall()
+            _pre = " | Book " + str(_bookTrack02) + " off " + str(_bookLength) + " | Author " + str(
+                _authorTrack) + " off " + str(
+                _authorLength)
             if _results.__len__() == 0:
                 _id = SQLExecute(_sqlConnection, "SELECT Max(authorID) + 1 FROM authors").fetchall()[0][0]
-                TimeTagMessage(" | Author not found in the table. Inserting into authors.\t | " + author)
+                TimeTagMessage(_pre + " | Author not found in the table. Inserting into authors.\t | " + author)
                 _sqlQuery = "INSERT INTO authors (authorID, firstName, lastName) VALUES (" + str(_id) + ", '" + \
                             _nameExtract[0] + "', '" + _nameExtract[1] + "')"
                 if _updateDatabasesQ:
@@ -81,4 +94,6 @@ if True:
                 elif not _updateDatabasesQ:
                     print(_sqlQuery + "\n")
             else:
-                TimeTagMessage(" | Author already exists in the table. Moving on.\t\t\t | " + author)
+                TimeTagMessage(_pre + " | Author already exists in the table. Moving on.\t\t\t | " + author)
+            _authorTrack += 1
+        _bookTrack02 += 1
