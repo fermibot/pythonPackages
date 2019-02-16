2977e8d927a41f019728071d05fdf99dd70cfe71
fermibot <ashwinuoh@gmail.com>
2019-02-16 01:30:40 -0600

    probably a working version of auto book, author and association update

--- mir_books_organization.py
+++ mir_books_organization.py
@@ -42,94 +42,80 @@ def nameExtract(_name: str):
 
 print("Found a total of " + str(_booksHave.__len__()) + "\n")
 
-for data in _booksHave:
-    _book = data[0]
-    _authors = Rest(data)
+_updateDatabasesQ = True
+_bookTrack01 = 1
+_bookLength = _booksHave.__len__()
 
+TimeTagMessage("Starting Books, Authors, Associations Upload")
 if True:
-    _updateDatabasesQ = True
+    for book in _booksHave:
+        authorList = Rest(book)
+        book = book[0]
+        _bookSQLResults = SQLExecute(_sqlConnection,
+                                     "SELECT * FROM books WHERE bookName = '" + book + "'").fetchall()
+        _prefix = " | Progress " + str(_bookTrack01) + " off " + str(_bookLength)
+        if _bookSQLResults.__len__() == 0:
+            _bookId = SQLExecute(_sqlConnection, "SELECT Max(bookID) + 1 FROM books").fetchall()[0][0]
+            TimeTagMessage(" | Progress " + str(_bookTrack01) + " off " + str(
+                _bookLength) + " | Book not found in the table. Inserting into books.\t | " + book)
+            _sqlQuery = "INSERT INTO books (bookID, bookName, series) VALUES (" + str(_bookId) + ", '" + \
+                        book + "', NULL)"
+            if _updateDatabasesQ:
+                _sqlConnection.cursor().execute(_sqlQuery)
+                _sqlConnection.commit()
+            elif not _updateDatabasesQ:
+                print(_sqlQuery + "\n")
+        else:
+            _bookId = _bookSQLResults[0][0]
+            TimeTagMessage(_prefix + " | Book already exists in the table. Moving on.\t\t\t\t\t\t | " + book)
+        _bookTrack01 += 1
 
-    _bookTrack01 = 1
-    _bookLength = _booksHave.__len__()
-    TimeTagMessage("Starting Books Upload")
-    if True:
-        for book in _booksHave:
-            book = book[0]
-            _bookSQLResults = SQLExecute(_sqlConnection,
-                                         "SELECT * FROM books WHERE bookName = '" + book + "'").fetchall()
-            _prefix = " | Progress " + str(_bookTrack01) + " off " + str(_bookLength)
-            if _bookSQLResults.__len__() == 0:
-                _bookId = SQLExecute(_sqlConnection, "SELECT Max(bookID) + 1 FROM books").fetchall()[0][0]
-                TimeTagMessage(" | Progress " + str(_bookTrack01) + " off " + str(
-                    _bookLength) + " | Book not found in the table. Inserting into books.\t | " + book)
-                _sqlQuery = "INSERT INTO books (bookID, bookName, series) VALUES (" + str(_bookId) + ", '" + \
-                            book + "', NULL)"
+        _authorTrack = 1
+        _authorLength = authorList.__len__()
+        for _author in authorList:
+            _nameExtract = nameExtract(_author)
+            _authorSQLResults = SQLExecute(_sqlConnection,
+                                           "SELECT * FROM authors WHERE firstName = '" +
+                                           _nameExtract[0] + "' AND lastName = '" + _nameExtract[1] + "'").fetchall()
+            _prefix = " | Book " + str(_bookTrack01) + " off " + str(_bookLength) + " | Author " + str(
+                _authorTrack) + " off " + str(
+                _authorLength)
+            if _authorSQLResults.__len__() == 0:
+                _authorId = SQLExecute(_sqlConnection, "SELECT Max(authorID) + 1 FROM authors").fetchall()[0][0]
+                TimeTagMessage(
+                    _prefix + " | Author not found in the table. Inserting into authors.\t | " + _author)
+                _sqlQuery = "INSERT INTO authors (authorID, firstName, lastName) VALUES (" + str(
+                    _authorId) + ", '" + \
+                            _nameExtract[0] + "', '" + _nameExtract[1] + "')"
                 if _updateDatabasesQ:
                     _sqlConnection.cursor().execute(_sqlQuery)
                     _sqlConnection.commit()
                 elif not _updateDatabasesQ:
                     print(_sqlQuery + "\n")
             else:
-                _bookId = _bookSQLResults[0][0]
-                TimeTagMessage(_prefix + " | Book already exists in the table. Moving on.\t\t | " + book)
-            _bookTrack01 += 1
+                _authorId = _authorSQLResults[0][0]
+                TimeTagMessage(_prefix + " | Author already exists in the table. Moving on.\t\t | " + _author)
 
-    print("")
-    TimeTagMessage("Starting Authors Upload")
-    if True:
-        _bookTrack02 = 1
-        for authorList in _booksHave:
-            authorList = Rest(authorList)
-            _authorTrack = 1
-            _authorLength = authorList.__len__()
-            for _author in authorList:
-                _nameExtract = nameExtract(_author)
-                _authorSQLResults = SQLExecute(_sqlConnection,
-                                               "SELECT * FROM authors WHERE firstName = '" +
-                                               _nameExtract[0] + "' AND lastName = '" + _nameExtract[
-                                                   1] + "'").fetchall()
-                _prefix = " | Book " + str(_bookTrack02) + " off " + str(_bookLength) + " | Author " + str(
-                    _authorTrack) + " off " + str(
-                    _authorLength)
-                if _authorSQLResults.__len__() == 0:
-                    _authorId = SQLExecute(_sqlConnection, "SELECT Max(authorID) + 1 FROM authors").fetchall()[0][0]
-                    TimeTagMessage(
-                        _prefix + " | Author not found in the table. Inserting into authors.\t | " + _author)
-                    _sqlQuery = "INSERT INTO authors (authorID, firstName, lastName) VALUES (" + str(
-                        _authorId) + ", '" + \
-                                _nameExtract[0] + "', '" + _nameExtract[1] + "')"
-                    if _updateDatabasesQ:
-                        _sqlConnection.cursor().execute(_sqlQuery)
-                        _sqlConnection.commit()
-                    elif not _updateDatabasesQ:
-                        print(_sqlQuery + "\n")
-                else:
-                    _authorId = _authorSQLResults[0][0]
-                    TimeTagMessage(_prefix + " | Author already exists in the table. Moving on.\t\t | " + _author)
+            _authorBookSQLResults = "SELECT * FROM authorBook WHERE bookFK =" + str(
+                _bookId) + " AND authorFK = " + str(_authorId)
+            _authorBookSQLResults = SQLExecute(_sqlConnection, _authorBookSQLResults).fetchall()
+            if _authorBookSQLResults.__len__() == 0:
+                print(
+                    _extraTab + "Association not found in the table. Now creating the link")
+                _getMaxId = "SELECT max(ID) + 1 FROM authorBook"
+                authorBookID = SQLExecute(_sqlConnection, _getMaxId).fetchall()[0][0]
+                authorBookID = ToString(authorBookID)
+                authorFK = ToString(_authorId)
+                bookFK = ToString(_bookId)
+                linkQuery = "INSERT INTO authorBook (ID, authorFK, bookFK) VALUES (" + authorBookID + ", " + authorFK + ', ' + bookFK + ")"
+                if _updateDatabasesQ:
+                    _sqlConnection.cursor().execute(linkQuery)
+                    _sqlConnection.commit()
+                elif not _updateDatabasesQ:
+                    print(linkQuery + "\n")
 
-                _authorBookSQLResults = "SELECT * FROM authorBook WHERE bookFK =" + str(
-                    _bookId) + " AND authorFK = " + str(_authorId)
-                _authorBookSQLResults = SQLExecute(_sqlConnection, _authorBookSQLResults).fetchall()
-                if _authorBookSQLResults.__len__() == 0:
-                    print(
-                        _extraTab + "Association not found in the table. Now creating the link")
-                    _getMaxId = "SELECT max(ID) + 1 FROM authorBook"
-                    authorBookID = SQLExecute(_sqlConnection, _getMaxId).fetchall()[0][0]
-                    authorBookID = ToString(authorBookID)
-                    authorFK = ToString(_authorId)
-                    bookFK = ToString(_bookId)
-                    linkQuery = "INSERT INTO authorBook (ID, authorFK, bookFK) VALUES (" + authorBookID + ", " + authorFK + ', ' + bookFK + ")"
-                    print(linkQuery);
-                    break
-                    if _updateDatabasesQ:
-                        _sqlConnection.cursor().execute(linkQuery)
-                        _sqlConnection.commit()
-                    elif not _updateDatabasesQ:
-                        print(linkQuery + "\n")
+            else:
+                print(
+                    _extraTab + "Association between this author and book found in the table. I am moving on!")
 
-                else:
-                    print(
-                        _extraTab + "Association between this author and book found in the table. I am moving on!")
-
-                _authorTrack += 1
-            _bookTrack02 += 1
+            _authorTrack += 1
