0568db7691103a0a422cba55e54ab458b1847ff8
fermibot <ashwinuoh@gmail.com>
2019-03-09 21:58:02 -0600

    backed up sql code

---
+++ mathematica/database_Queries.sql
@@ -0,0 +1,30 @@
+SELECT
+	bookName                        Book,
+	firstName || ' ' || lastName AS Author,
+	series                          BookSeries
+FROM books
+	LEFT OUTER JOIN authorBook a ON books.bookID = a.bookFK
+	LEFT OUTER JOIN authors ON authorFK = authorID
+WHERE Author IS NOT NULL
+ORDER BY BookSeries, Book, Author;
+
+-- SELECT
+-- 	coin.denominationName,
+-- 	n.denominationSeries,
+-- 		geography.geoLocation,
+-- 	denominationYear,
+-- 	denominationHave
+-- FROM numismatics_master coin
+-- 	LEFT OUTER JOIN geography ON geography.geoCode = coin.denominationGeoCode
+-- 	LEFT OUTER JOIN numismatics_series n ON coin.denominationSeriesId = n.denominationSeriesId
+-- WHERE denominationHave = 0
+-- ORDER BY n.denominationSeries, denominationName;
+
+
+-- DELETE FROM authors
+-- WHERE authorID > 61;
+--
+-- DELETE FROM books
+-- WHERE bookid > 62;
+--
+-- DELETE FROM authorBook WHERE ID > 68;
