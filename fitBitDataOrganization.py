47874900eb4850482a51d0bcef39ae360d099faa
fermibot <ashwinuoh@gmail.com>
2019-04-02 21:23:19 -0500

    started addition of the functionality for the distance logFiles

--- fitBitDataOrganization.py
+++ fitBitDataOrganization.py
@@ -25,6 +25,10 @@ def altitudeExtractor(data: dict):
     return [data['dateTime'], data['value']]
 
 
+def distanceExtractor(data: dict):
+    return [data['dateTime'], data['value']]
+
+
 class altitudeClass:
     def tableCheck(self, _dateMin, _dateMax):
         return "SELECT * FROM biometricsAltitude WHERE dateTimeID IN (\'" + _dateMin + "\', \'" + _dateMax + "\')"
@@ -34,6 +38,17 @@ class altitudeClass:
 
     def inserter(self, data: dict):
         return "INSERT INTO biometricsAltitude VALUES ('" + data['dateTime'] + "', " + str(data['value']) + ")"
+
+
+class distanceClass:
+    def tableCheck(self, _dateMin, _dateMax):
+        return "SELECT * FROM biometricsDistance WHERE dateTimeID IN (\'" + _dateMin + "\', \'" + _dateMax + "\')"
+
+    def recordCheck(self, data: dict):
+        return "SELECT * FROM biometricsDistance WHERE dateTimeID = \'" + data['dateTime'] + "\'"
+
+    def inserter(self, data: dict):
+        return "INSERT INTO biometricsDistance VALUES ('" + data['dateTime'] + "', " + str(data['value']) + ")"
 
 
 class heartRateClass:
@@ -50,6 +65,7 @@ class heartRateClass:
 
 heartRate = heartRateClass()
 altitude = altitudeClass()
+distance = distanceClass()
 
 mD = {'m0': 'TimeElapsed', 'm1': 'Processing file#', 'm2': 'Processing line number',
       'm3': 'record not found in the database, now inserting',
@@ -65,68 +81,101 @@ _fCt = 1
 metricType = {'altitude-': 9, 'heart_rate-': 11}
 
 with open(f"D:\Programming\_databases\{'altitude'}.txt", 'w+') as altitudeLogFile, \
-        open(f"D:\Programming\_databases\{'heart_rate'}.txt", 'w+') as heart_rateLogFile:
+        open(f"D:\Programming\_databases\{'heart_rate'}.txt", 'w+') as heart_rateLogFile, \
+        open(f"D:\Programming\_databases\{'distance'}.txt", 'w+') as distanceLogFile:
     for path, item, files in os.walk(directory):
         for file in files:
-            if file[:9] == 'altitude-':
+            if file[:9] == 'distance-':
                 with open(path + '\\' + file) as inJsonFile:
                     inJsonData = json.load(inJsonFile)
                     [_dtMin, _dtMax] = [inJsonData[0]['dateTime'], inJsonData[-1]['dateTime']]
                     _timeDelta = timeDelta(startTime)
-                    _tableCheck = _sqlConnection.execute(altitude.tableCheck(_dtMin, _dtMax)).fetchall()
+                    _tableCheck = _sqlConnection.execute(distance.tableCheck(_dtMin, _dtMax)).fetchall()
                     if len(_tableCheck) == 2:
                         sys.stdout.write(f"\r{_timeDelta}::FileCount {_fCt}::{mD['m8']}::{file}")
                         time.sleep(0.01)
                     if len(_tableCheck) < 2:
                         _rCt = 0
                         for record in inJsonData:
-                            line = altitudeExtractor(record)
-                            _sqlResults = _sqlConnection.execute(altitude.recordCheck(record)).fetchall()
+                            line = distanceExtractor(record)
+                            _sqlResults = _sqlConnection.execute(distance.recordCheck(record)).fetchall()
                             _messagePrefix = f"{mD['m0']} {_timeDelta}::FileCount {_fCt}::{mD['m2']} {_rCt}"
                             if len(_sqlResults) == 0:
                                 sys.stdout.write(f"\r{_messagePrefix}::{mD['m3']}")
-                                _insertQuery = altitude.inserter(record)
+                                _insertQuery = distance.inserter(record)
                                 _sqlConnection.cursor().execute(_insertQuery)
                                 _sqlConnection.commit()
                             elif len(_sqlResults) == 1:
                                 sys.stdout.write(f"\r{_messagePrefix}::{mD['m5']}")
                             elif len(_sqlResults) > 1:
                                 sys.stdout.write(f"\r{_messagePrefix}::{mD['m4']}")
-                                altitudeLogFile.write(f"{mD['m4']}:: {line[0]}.")
+                                distanceLogFile.write(f"{mD['m4']}:: {line[0]}.")
                             _rCt += 1
                         inJsonFile.close()
                     if len(_tableCheck) > 2:
                         sys.stdout.write(f"\rSeems like there is an issue with this file. {mD['m6']}")
-                        altitudeLogFile.write(f"{mD['m7']}::{file}")
+                        distanceLogFile.write(f"{mD['m7']}::{file}")
                 _fCt += 1
+            if False:
+                if file[:9] == 'altitude-':
+                    with open(path + '\\' + file) as inJsonFile:
+                        inJsonData = json.load(inJsonFile)
+                        [_dtMin, _dtMax] = [inJsonData[0]['dateTime'], inJsonData[-1]['dateTime']]
+                        _timeDelta = timeDelta(startTime)
+                        _tableCheck = _sqlConnection.execute(altitude.tableCheck(_dtMin, _dtMax)).fetchall()
+                        if len(_tableCheck) == 2:
+                            sys.stdout.write(f"\r{_timeDelta}::FileCount {_fCt}::{mD['m8']}::{file}")
+                            time.sleep(0.01)
+                        if len(_tableCheck) < 2:
+                            _rCt = 0
+                            for record in inJsonData:
+                                line = altitudeExtractor(record)
+                                _sqlResults = _sqlConnection.execute(altitude.recordCheck(record)).fetchall()
+                                _messagePrefix = f"{mD['m0']} {_timeDelta}::FileCount {_fCt}::{mD['m2']} {_rCt}"
+                                if len(_sqlResults) == 0:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m3']}")
+                                    _insertQuery = altitude.inserter(record)
+                                    _sqlConnection.cursor().execute(_insertQuery)
+                                    _sqlConnection.commit()
+                                elif len(_sqlResults) == 1:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m5']}")
+                                elif len(_sqlResults) > 1:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m4']}")
+                                    altitudeLogFile.write(f"{mD['m4']}:: {line[0]}.")
+                                _rCt += 1
+                            inJsonFile.close()
+                        if len(_tableCheck) > 2:
+                            sys.stdout.write(f"\rSeems like there is an issue with this file. {mD['m6']}")
+                            altitudeLogFile.write(f"{mD['m7']}::{file}")
+                    _fCt += 1
 
-            if file[:11] == 'heart_rate-':
-                with open(path + '\\' + file) as inJsonFile:
-                    inJsonData = json.load(inJsonFile)
-                    [_dtMin, _dtMax] = [inJsonData[0]['dateTime'], inJsonData[-1]['dateTime']]
-                    _timeDelta = timeDelta(startTime)
-                    _tableCheck = _sqlConnection.execute(heartRate.tableCheck(_dtMin, _dtMax)).fetchall()
-                    if len(_tableCheck) == 2:
-                        sys.stdout.write(f"\r{_timeDelta}::FileCount {_fCt}::{mD['m8']}::{file}")
-                        time.sleep(0.01)
-                    if len(_tableCheck) < 2:
-                        _rCt = 0
-                        for record in inJsonData:
-                            line = heartRateExtractor(record)
-                            _sqlResults = _sqlConnection.execute(heartRate.recordCheck(record)).fetchall()
-                            _messagePrefix = f"{mD['m0']} {_timeDelta}::FileCount {_fCt}::{mD['m2']} {_rCt}"
-                            if len(_sqlResults) == 0:
-                                sys.stdout.write(f"\r{_messagePrefix}::{mD['m3']}")
-                                _sqlConnection.cursor().execute(heartRate.inserter(record))
-                                _sqlConnection.commit()
-                            elif len(_sqlResults) == 1:
-                                sys.stdout.write(f"\r{_messagePrefix}::{mD['m5']}")
-                            elif len(_sqlResults) > 1:
-                                sys.stdout.write(f"\r{_messagePrefix}::{mD['m4']}")
-                                heart_rateLogFile.write(f"{mD['m4']}:: {line[0]}.")
-                            _rCt += 1
-                        inJsonFile.close()
-                    if len(_tableCheck) > 2:
-                        sys.stdout.write(f"\rSeems like there is an issue with this file. {mD['m6']}")
-                        heart_rateLogFile.write(f"{mD['m7']}::{file}")
-                _fCt += 1
+                if file[:11] == 'heart_rate-':
+                    with open(path + '\\' + file) as inJsonFile:
+                        inJsonData = json.load(inJsonFile)
+                        [_dtMin, _dtMax] = [inJsonData[0]['dateTime'], inJsonData[-1]['dateTime']]
+                        _timeDelta = timeDelta(startTime)
+                        _tableCheck = _sqlConnection.execute(heartRate.tableCheck(_dtMin, _dtMax)).fetchall()
+                        if len(_tableCheck) == 2:
+                            sys.stdout.write(f"\r{_timeDelta}::FileCount {_fCt}::{mD['m8']}::{file}")
+                            time.sleep(0.01)
+                        if len(_tableCheck) < 2:
+                            _rCt = 0
+                            for record in inJsonData:
+                                line = heartRateExtractor(record)
+                                _sqlResults = _sqlConnection.execute(heartRate.recordCheck(record)).fetchall()
+                                _messagePrefix = f"{mD['m0']} {_timeDelta}::FileCount {_fCt}::{mD['m2']} {_rCt}"
+                                if len(_sqlResults) == 0:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m3']}")
+                                    _sqlConnection.cursor().execute(heartRate.inserter(record))
+                                    _sqlConnection.commit()
+                                elif len(_sqlResults) == 1:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m5']}")
+                                elif len(_sqlResults) > 1:
+                                    sys.stdout.write(f"\r{_messagePrefix}::{mD['m4']}")
+                                    heart_rateLogFile.write(f"{mD['m4']}:: {line[0]}.")
+                                _rCt += 1
+                            inJsonFile.close()
+                        if len(_tableCheck) > 2:
+                            sys.stdout.write(f"\rSeems like there is an issue with this file. {mD['m6']}")
+                            heart_rateLogFile.write(f"{mD['m7']}::{file}")
+                    _fCt += 1
