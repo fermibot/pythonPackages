24ce022823c9e160dcfc62589c058ce8251a17cd
fermibot <ashwinuoh@gmail.com>
2019-04-27 13:03:32 -0500

    added yearReformat to the fitbitDataOrganization

--- fitbitDateReformatting.py
+++ fitbitDateReformatting.py
@@ -1,19 +1,30 @@
 import glob
 import csv
 import sys
+import re
 
 
-def yearReplace(dateTimeID: str) -> str:
-    return dateTimeID.replace('/18 ', '/2018 ').replace('/19 ', '/2019 ')
+def yearReplace(dateString: str) -> str:
+    if dateString.split('\t')[0].__len__() != 10:
+        time = re.findall('..:..:..', dateString)[0]
+        return dateString.replace('/18 ', '/2018 ').replace('/19 ', '/2019 ').replace('/', '-').replace(time,
+                                                                                                        time)
 
 
 tsvDirectory = "D:\\Programming\\_databases\\fitbitDateReformatting\\"
 
-for file in glob.glob(tsvDirectory + "main_*.tsv"):
-    lineTrack = 1
-    with open(file) as tsvInFile, \
-            open(file.replace('.tsv', '_reformatted.tsv').replace('main_', 'main-'), 'w') as tsvOutFile:
-        for line in tsvInFile:
-            sys.stdout.write(f"\rNow processing file::{file} line#::{lineTrack}")
-            tsvOutFile.write(yearReplace(line))
-            lineTrack += 1
+
+def reformatAllFiles():
+    for file in glob.glob(tsvDirectory + "main_*.tsv"):
+        lineTrack = 1
+        with open(file) as tsvInFile, \
+                open(file.replace('.tsv', '_reformatted.tsv').replace('main_', 'main-'), 'w') as tsvOutFile:
+            for line in tsvInFile:
+                if line.split('\t')[0].__len__() != 10:
+                    sys.stdout.write(f"\rNow processing file::{file} line#::{lineTrack}")
+                    tsvOutFile.write(yearReplace(line))
+                    lineTrack += 1
+
+
+if __name__ == '__main__':
+    reformatAllFiles()
