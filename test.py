096aa0302a424f0a42b1d7b7b67373568f1bf671
fermibot <ashwinuoh@gmail.com>
2020-01-05 17:29:54 -0600

    Algorithms exploration section, with Mr. Vardhan Adabala.

--- test.py
+++ test.py
@@ -1,15 +1,26 @@
-def countApplesAndOranges(s, t, a, b, apples, oranges):
-    apples_count = 0
-    oranges_count = 0
-    for apple in apples:
-        if s <= a + apple <= t:
-            apples_count += 1
-    for orange in oranges:
-        if s <= b + orange <= t:
-            oranges_count += 1
-    print(apples_count)
-    print(oranges_count)
+def factorial(x: int):
+    _factorial = 1
+    for i in range(1, x + 1):
+        _factorial *= i
+    return _factorial
+
+
+def sine_cos(x, one_or_zero: 1, precision: 50):
+    _sine_cos = 0
+    for i in range(0, precision):
+        exp_fact = 2 * i + one_or_zero
+        _sine_cos += ((-1) ** i) * (x ** exp_fact) / factorial(exp_fact)
+    return _sine_cos
+
+
+def sin(x, precision: 50):
+    return sine_cos(x, one_or_zero=1, precision=precision)
+
+
+def cos(x, precision: 50):
+    return sine_cos(x, one_or_zero=0, precision=precision)
 
 
 if __name__ == '__main__':
-    countApplesAndOranges()
+    for i in range(50):
+        print([cos(10, precision=i), sin(10, precision=i)])
